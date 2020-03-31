c63a19f8eab185efdea1ffa1790859d57f03aeec
munibkhan <munibk38@gmail.com>
2020-03-31 11:16:46 +0000

    deleted folder 31-March-2020

--- 31-March-2020/wordpress/wp-content/plugins/trx_addons/components/api/siteorigin-panels/params/icons/icons.js
+++
@@ -1,8 +0,0 @@
-/* global jQuery:false */
-
-(function() {
-	"use strict";
-	jQuery(document).on( 'sowsetupformfield', '.siteorigin-widget-field-type-icons', function(e) {
-		jQuery(document).trigger('action.init_hidden_elements', [jQuery('.so-panels-dialog')]);
-	});
-})();
